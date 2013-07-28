344bfcd9b52748305d2bb3b5ca6e512b5631bd80
Matthew Makai <makaimc@gmail.com>
2013-07-28 13:18:02 -0400

    Updating miles driven to today.

--- theme/js/caa.js
+++ theme/js/caa.js
@@ -394,7 +394,7 @@ function drawCitiesMap() {
         .bindPopup('<span class="city-name"><a href="/madison-wi.html">Madison, WI</a><br/>July 8-14, 2013</span><br/><span class="future-stories">Drifty</span>');
     
     L.marker(new L.LatLng(41.85, -87.65)).addTo(map)
-        .bindPopup('<span class="city-name"><a href="/chicago-il.html">Chicago, IL</a><br/>July 14-21, 2013</span><br/><a href="/chicago-pictures.html">Chicago Pictures</a><br/><span class="future-stories">1871<br/>The Starter League<br/>Matchist<br/><br/><span class="external-resources">External Resources</span><br/><a href="http://technori.com/" target="_blank">Technori</a><br/><a href="http://www.builtinchicago.org/" target="_blank">Built in Chicago</a><br/>');
+        .bindPopup('<span class="city-name"><a href="/chicago-il.html">Chicago, IL</a><br/>July 14-21, 2013</span><br/><a href="/chicago-pictures.html">Chicago Pictures</a><br/><span class="future-stories">1871<br/>The Starter League<br/>Matchist<br/><br/><span class="external-resources">External Resources</span><br/><a href="http://techweek.com/chicago/">Techweek Chicago</a><br/><a href="http://startupdigest.com/chicago/">StartupDigest Chicago</a></br><a href="http://technori.com/" target="_blank">Technori</a><br/><a href="http://www.builtinchicago.org/" target="_blank">Built in Chicago</a><br/>');
     
     L.marker(new L.LatLng(42.33, -83.05)).addTo(map)
         .bindPopup('<span class="city-name"><a href="/detroit-mi.html">Detroit, MI</a><br/>July 21-26, 2013</span>');
